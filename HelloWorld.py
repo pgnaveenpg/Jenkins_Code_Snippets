3fbb7068da48a2444d5cd3f00f8a7c5167682aae
Naveen PG <66114170+pgnaveenpg@users.noreply.github.com>
2021-09-06 21:37:07 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Enta ssaavvvaaa... idu build periodically mrreee pop? !!!\n")
+print("Enta ssaavvvaaa... idu build poll scm !!!\n")
 
 
 
